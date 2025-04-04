4f67656409be3d871f19f2d82115f1c1158fd822
Kati <mayssketii@gmail.com>
2025-04-04 20:53:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int startingScore = 100; // начальный счет
+        int replenishment = 1100; // сумма пополнения
+
+        int bonus;
+        if (replenishment > 1100) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+    }
+
+}
