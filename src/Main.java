148a8509a7debf54df5912e578d83afc89c42d37
Kati <mayssketii@gmail.com>
2025-04-05 08:40:10 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int startingScore = 100; // начальный счет
-        int replenishment = 1100; // сумма пополнения
+        int replenishment = 1000; // сумма пополнения
 
         int bonus;
-        if (replenishment > 1100) {
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
         } else {
             bonus = 0;
